487dafaf67b49e9e6c01c1f16246c43f17533356
DanielCater <d29cate@siena.edu>
2026-02-12 19:48:34 -0500

    Updated generator inputs

--- src/main/java/com/mycompany/passwordapp/Main.java
+++ src/main/java/com/mycompany/passwordapp/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         // Example usage of PasswordGenerator
-        String password = PasswordGenerator.generator(20);
+        String password = PasswordGenerator.generator(20, true, true, true, true);
         System.out.println("Generated Password: " + password);
 
         // Example usage of PasswordAnalyzer

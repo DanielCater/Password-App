39f9af2c3dc590076493644a1120518a177dd7f6
DanielCater <d29cate@siena.edu>
2026-02-12 19:53:21 -0500

    Updated comments

--- src/main/java/com/mycompany/passwordapp/Main.java
+++ src/main/java/com/mycompany/passwordapp/Main.java
@@ -1,5 +1,12 @@
 package com.mycompany.passwordapp;
 
+/**
+ * @author Daniel Cater
+ * @version 2/12/2026
+ * 
+ * This class allows for the testing of the generator and analyzer classes,
+ * which are the backend of the password generator app.
+ */
 public class Main {
 
     public static void main(String[] args) {

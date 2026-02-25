f78ec006406a669ff38fc28c2c56abec707a740e
DanielCater <d29cate@siena.edu>
2026-02-24 19:06:48 -0500

    Passwords now include at least one of each character type selected.

--- src/main/java/com/mycompany/passwordapp/PasswordGenerator.java
+++ src/main/java/com/mycompany/passwordapp/PasswordGenerator.java
@@ -1,6 +1,9 @@
 package com.mycompany.passwordapp;
 
 import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author Daniel Cater
@@ -31,18 +34,53 @@ public class PasswordGenerator {
     public static String generator(int length, boolean lower, boolean upper, boolean nums, boolean syms) {
         int index;
         StringBuilder validChars = new StringBuilder();
+        List<String> requiredPools = new ArrayList<>();
+
         
         if(!lower && !upper && !nums && !syms) return "Must select character type(s)";
-        if(lower) validChars.append(LOWER);
-        if(upper) validChars.append(UPPER);
-        if(nums) validChars.append(DIGITS);
-        if(syms) validChars.append(SYMBOLS);
+        if(lower){
+            validChars.append(LOWER);
+            requiredPools.add(LOWER);
+        }
+        if(upper){
+            validChars.append(UPPER);
+            requiredPools.add(UPPER);
+        }
+        if(nums){
+            validChars.append(DIGITS);
+            requiredPools.add(DIGITS);
+        }
+        if(syms){
+            validChars.append(SYMBOLS);
+            requiredPools.add(SYMBOLS);
+        }
+        
+        if(length < requiredPools.size()){
+            return "Length must be at least the same as number of types selected!";
+        }
         
         StringBuilder password = new StringBuilder(length);
-        for (int i = 0; i < length; i++) {
+        
+        for (String pool : requiredPools) {
+            password.append(pool.charAt(random.nextInt(pool.length())));
+        }
+        
+        for (int i = requiredPools.size(); i < length; i++) {
             index = random.nextInt(validChars.length());
             password.append(validChars.charAt(index));
         }
-        return password.toString();
+        
+        List<Character> chars = new ArrayList<>();
+        for (char c : password.toString().toCharArray()) {
+            chars.add(c);
+        }
+        Collections.shuffle(chars, random);
+
+        StringBuilder result = new StringBuilder();
+        for (char c : chars) {
+            result.append(c);
+        }
+
+        return result.toString();
     }
 }
